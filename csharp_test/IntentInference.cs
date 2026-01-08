514bbee0e95e9278e4db74ba1d8adc346aa764f2
Addy <aclaure@gmail.com>
2026-01-08 08:13:31 -0500

    this code needs to be tested but I don't have time right now;

--- csharp_test/IntentInference.cs
+++ csharp_test/IntentInference.cs
@@ -2,8 +2,58 @@
 using Microsoft.ML.OnnxRuntime.Tensors;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Text.Json;
+
+public class SoftMaxResult
+{
+    public IReadOnlyList<double> RawScores { get; }
+    public IReadOnlyList<double> Probabilities { get; }
+    public int PredictedClassIndex { get; }
+    public double MaxProbability { get; }
+    public double MaxRawScore { get; }
+    public IReadOnlyList<(int Index, double Probability)> SortedProbabilities { get; }
+
+    public SoftMaxResult(IReadOnlyList<double> rawScores)
+    {
+        this.RawScores = rawScores;
+        this.MaxRawScore = RawScores.Max();
+
+        // Subtract max for numerical stability. This prevents overflow in exp.
+        var expScores = rawScores.Select(score => Math.Exp(score - MaxRawScore)).ToList();
+        var sumExpScores = expScores.Sum();
+
+        this.Probabilities = expScores.Select(expScore => expScore / sumExpScores).ToList();
+        this.MaxProbability = this.Probabilities.Max();
+        this.PredictedClassIndex = this.Probabilities.Select((value, index) => (value, index)).Max().index;
+
+        this.SortedProbabilities = this.Probabilities
+            .Select((value, index) => (Index: index, Probability: value))
+            .OrderByDescending(item => item.Probability)
+            .ToList();
+    }
+
+    public override string ToString()
+    {
+        // Number of top entries to show
+        int topN = Math.Min(3, SortedProbabilities.Count);
+
+        // Build top entries string
+        var topEntries = SortedProbabilities
+            .Take(topN)
+            .Select((item, rank) => $"{rank + 1}. idx={item.Index}, prob={item.Probability:P2}")
+            .ToArray();
+        string topPart = topEntries.Length > 0 ? string.Join(", ", topEntries) : "none";
+
+        // If there are more classes, indicate total count
+        string moreInfo = SortedProbabilities.Count > topN
+            ? $" (showing top {topN} of {SortedProbabilities.Count})"
+            : string.Empty;
+
+        return $"Predicted: {PredictedClassIndex} ({MaxProbability:P2}), RawMax: {MaxRawScore:G6}\nTop: {topPart}{moreInfo}";
+    }
+}
 
 public class IntentResult
 {
@@ -16,6 +66,19 @@ public class IntentResult
         IntentLogits = intent;
         SpeedLogits = speed;
         UrgencyLogits = urgency;
+    }
+
+    private float _softSum = -1;
+
+    List<float[]> SoftMaxResults()
+    {
+        List<float[]> result = new List<float[]>();
+
+        _softSum = IntentLogits.Select(x => (float)Math.Pow(Math.E, x)).Sum();
+        var intentSoftmax = IntentLogits.Select(x => (float)(Math.Pow(Math.E, x) / _softSum)).ToList();
+        var score = intentSoftmax.Max();
+
+        return result;
     }
 }
 
--- csharp_test/README.md
+++ csharp_test/README.md
@@ -1,1 +1,9 @@
+# Microsoft ML.NET ONNX Runtime
+
+https://www.nuget.org/packages/Microsoft.ML.OnnxRuntime
+
+## Installation
+
+```bash
 dotnet add package Microsoft.ML.OnnxRuntime --version 1.23.2
+```
