f68b99eca58c1fc75acd5112300d20396d8d0126
Addy <aclaure@gmail.com>
2026-01-08 20:35:02 -0500

    more flexible model folder;

--- csharplib/Program.cs
+++ csharplib/Program.cs
@@ -2,20 +2,27 @@
 
 class Program
 {
+    const string MODEL_PATH = @"/Users/addy/src/dfs/saintaveline-intent-model/data";
+    // const string MODEL_PATH = @"S:\dfs\saintaveline-intent-model";
+    
     static void Main(string[] args)
     {
         IntentInference inf = new IntentInference(
-            @"S:\dfs\saintaveline-intent-model\intent_model.onnx",
-            @"S:\dfs\saintaveline-intent-model\vocab.json"
+            MODEL_PATH + Path.DirectorySeparatorChar + "intent_model.onnx",
+            MODEL_PATH + Path.DirectorySeparatorChar + "vocab.json"
         );
 
-        IntentResult r = inf.Run("Give Matthew a blowie in the bathroom!");
+        IntentResult r = inf.Run("Walk to Central Park now!");
 
+        // var intentSoftmax = new SoftMaxResult([.. r.IntentLogits.Select(x => (double)x)]); // C#11 or greater
         var intentSoftmax = new SoftMaxResult(r.IntentLogits.Select(x => (double)x).ToArray());
-        //Console.WriteLine($"Result: {intentSoftmax.ToString()}");  
-        string str = intentSoftmax.ToString();
-        //string str = "HELLO MATTHEW AND KERBY";
-        Console.WriteLine("str: " + str);
+        Console.WriteLine("Intent: " + intentSoftmax.ToString());
+
+        var speedSoftmax = new SoftMaxResult(r.SpeedLogits.Select(x => (double)x).ToArray());
+        Console.WriteLine("Speed: " + speedSoftmax.ToString());
+        
+        var urgencySoftmax = new SoftMaxResult(r.UrgencyLogits.Select(x => (double)x).ToArray());
+        Console.WriteLine("Urgency: " + urgencySoftmax.ToString());
 
         Console.WriteLine("Hello, World!");
     }
